fc8ec8a381e52c96ba5f1018e2a2c36adaf73795
andrejcermak <andrej.cermak.1999@gmail.com>
2025-04-03 15:38:52 +0200

    Removing credentials from coder parameters file

--- coder_parameters.tf
+++ coder_parameters.tf
@@ -5,27 +5,6 @@ data "coder_parameter" "project_id" {
   mutable     = false
   default     = "e495700ad00349bab1d6c75fa13d0ad1"
 }
-data "coder_parameter" "application_credential_name" {
-  name        = "application_credential_name"
-  description = "application_credential_name"
-  type        = "string"
-  mutable     = false
-  default = "just_reader"
-}
-data "coder_parameter" "application_credential_id" {
-  name        = "application_credential_id"
-  description = "application_credential_id"
-  type        = "string"
-  mutable     = false
-  default     = "34e9dd18dfcc4208bb8baed1d1cc9533" 
-}
-data "coder_parameter" "application_credential_secret" {
-  name        = "application_credential_secret"
-  description = "application_credential_secret"
-  type        = "string"
-  mutable     = false
-  default     = "ScPOaem7j-aKQUPF2UEdpQj8dKu4GuhfjfkIC6KRyh5G5DpR-yYMlS1-dsfCkXPIWVPBOdhLiyAjAY3PjAHwhA"
-}
 
 data "coder_parameter" "pubkey"{
   name        = "pubkey"
