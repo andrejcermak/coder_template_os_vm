a17ef90eac4d704696d41b26564e9074f594f7aa
andrejcermak <andrej.cermak.1999@gmail.com>
2025-05-13 15:07:36 +0200

    Adding input variables for default coder parameters

--- coder_parameters.tf
+++ coder_parameters.tf
@@ -1,16 +1,55 @@
+variable "application_credential_id" {
+  type = string
+}
+
+variable "application_credential_name"{
+  type = string
+}
+
+variable "application_credential_secret"{
+  type = string
+}
+
+variable "openstack_region"{
+ type = string
+}
+
+variable "openstack_identity_provider" {
+  type = string
+}
+
+
+variable "project_id" {
+  type = string
+}
+
 data "coder_parameter" "project_id" {
   name        = "project_id"
   description = "Project id"
   type        = "string"
   mutable     = false
-  default     = "e495700ad00349bab1d6c75fa13d0ad1"
+  default     = var.project_id
 }
-
-data "coder_parameter" "pubkey"{
-  name        = "pubkey"
-  description = "pubkey"
+data "coder_parameter" "application_credential_name" {
+  name        = "application_credential_name"
+  description = "application_credential_name"
   type        = "string"
-  mutable     = false
+  mutable     = true
+  default = var.application_credential_name
+}
+data "coder_parameter" "application_credential_id" {
+  name        = "application_credential_id"
+  description = "application_credential_id"
+  type        = "string"
+  mutable     = true
+  default     = var.application_credential_id
+}
+data "coder_parameter" "application_credential_secret" {
+  name        = "application_credential_secret"
+  description = "application_credential_secret"
+  type        = "string"
+  mutable     = true
+  default     = var.application_credential_secret
 }
 
 data "coder_parameter" "openstack_region"{
@@ -18,12 +57,19 @@ data "coder_parameter" "openstack_region"{
   description = "openstack_region"
   type        = "string"
   mutable     = false
-  default     = "Brno1"
+  default     = var.openstack_region
 }
 data "coder_parameter" "openstack_identity_provider"{
   name        = "openstack_identity_provider"
   description = "openstack_identity_provider"
   type        = "string"
   mutable     = false
-  default     = "https://identity.brno.openstack.cloud.e-infra.cz/v3"
+  default     = var.openstack_identity_provider
+}
+
+data "coder_parameter" "pubkey"{
+  name        = "pubkey"
+  description = "pubkey"
+  type        = "string"
+  mutable     = false
 }
